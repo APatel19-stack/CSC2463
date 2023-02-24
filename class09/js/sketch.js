84d0809645209a62ee0a8616daf469a332bb0491
Apate19 <72895811+APatel19-stack@users.noreply.github.com>
2023-02-23 21:24:26 -0600

    update

--- class09/js/sketch.js
+++ class09/js/sketch.js
@@ -1,9 +1,11 @@
 //let sound1 = new Tone.Player("Sounds/ ..........");
 
-let sounds = new Tone.Player({
-    "01": "sounds/01.mp3",
-    "02": "sounds/02.mp3",
-    "03": "sounds/03.mp3"
+let sounds = new Tone.Players({
+    '01': 'Sounds/chicken.wav',
+    '02': 'Sounds/water.mp3',
+    '03': 'Sounds/ductTape.wav',
+    '04': 'Sounds/trap_loop.mp3'
+
 
 });
 
@@ -12,7 +14,7 @@ const delay = new Tone.FeedbackDelay("8n", 0.5);
 let button1;
 let button2;
 let button3;
-let SoundNames = ["01", "02", "03"];
+let SoundNames = ['01', '02', '03', '04'];
 let buttons = [];
 
 let dslider;
@@ -21,26 +23,30 @@ let fslider;
 
 function setup() {
     createCanvas(400, 400);
+    // sounds.toDestination();
     sounds.connect(delay);
     delay.toDestination();
 
 
     SoundNames.forEach((word, index) => {
-        buttons[index] = createButton(word);
-        buttons[index].position(index, index * 58);
+        buttons[index] = createButton("Sound track: " + word);
+        buttons[index].size(150, 25);
+        buttons[index].position(index + 120, (index + 1) * 50);
         buttons[index].mousePressed(() => buttonSound(word));
-
     })
 
     dslider = createSlider(0, 1., 0.5, 0.05);
     dslider.mouseReleased(() => {
         delay.delayTime.value = dslider.value();
     })
+    dslider.position(30, buttons.length * 72)
+
 
     fslider = createSlider(0, 1., 0.5, 0.05);
     fslider.mouseReleased(() => {
         delay.feedback.value = fslider.value();
     })
+    fslider.position(230, buttons.length * 72)
 
 
 
@@ -49,46 +55,16 @@ function setup() {
 function draw() {
     background(220);
 
-    text("Press button for sound", 0, 150)
-
-    // button1 = creatButton("");
-    // button1.position(50, 50);
-    // //() => buffer functions
-    // button1.mousePressed(() => buttonSound("Sound name"));
-
-    // button2 = creatButton("");
-    // button2.position(50, 50);
-    // button2.mousePressed(() => buttonSound("Sound name"));
-
-    // button3 = creatButton("");
-    // button3.position(50, 50);
-    // button3.mousePressed(() => buttonSound("Sound name"));
+    text("Press button for sound", 135, buttons.length * 60)
+    text("Delay effect", 40, buttons.length * 70)
+    text("Feedback effect", 240, buttons.length * 70)
 
 }
 
 function buttonSound(whichSound) {
 
     sounds.player(whichSound).start();
-
-    // if (whichSound === "i") {
-    //     //sound1.playbackRate = ((mouseY / 200) + 0.01);
-    //     sounds.player("Sound name").start();
-    // } else if (whichSound === "i") {
-    //     //sound1.playbackRate = ((mouseY / 200) + 0.01);
-    //     sounds.player("Sound name").start();
-    // } else if (whichSound === "i") {
-    //     //sound1.playbackRate = ((mouseY / 200) + 0.01);
-    //     sounds.player("Sound name").start();
-    // }
+    //    sounds.player(whichSound).start();
 
 
 }
-
-
-// function keyPressed() {
-//     if (key === "i") {
-//         sound1.playbackRate = ((mouseY / 200) + 0.01);
-//         sound1.start();
-//     }
-
-// }
